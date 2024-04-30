037a35d790d2963449dbae98bcfefd776e4977d3
Adrephos <adrephos@gmail.com>
2024-04-29 20:02:36 -0500

    feat: add inputs components

---
+++ src/components/Inputs.tsx
@@ -0,0 +1,55 @@
+let inputStyle = "bg-[#232327] border border-[#47474F] placeholder-[#5C5C67] text-white text-sm rounded-lg block w-[28rem] p-2.5 "
+
+export const StringInput = ({ name, setVal }: { name: string, setVal: Function }) => {
+  return (
+    <div>
+      <input placeholder={name} className={inputStyle}
+        onChange={(e) => setVal(e.target.value)}
+      />
+    </div>
+  )
+}
+
+export const NumberInput = ({ name, setVal, type }: { name: string, setVal: Function, type: string }) => {
+  return (
+    <div>
+      <input placeholder={name} className={inputStyle}
+        onChange={(e) => setVal(
+          type === "int"
+            ? parseInt(e.target.value) || 0
+            : parseFloat(e.target.value) || 0
+        )}
+      />
+    </div>
+  )
+}
+
+export const ToggleInput = (
+  { a, b, stateVal, setStateVal }:
+    { a: string, b: string, stateVal: boolean, setStateVal: Function }
+) => {
+  return (
+    <div>
+      <label
+        htmlFor="toggle"
+        className="inline-flex items-center rounded-md cursor-pointer"
+      >
+        <input
+          id="toggle"
+          type="checkbox"
+          className="hidden peer"
+          checked={stateVal}
+          onChange={() => setStateVal(!stateVal)} />
+        <span className="px-4 py-[0.75rem] rounded-l-md bg-[#44279A]
+        text-white peer-checked:bg-[#2B2931]"
+        >
+          {a}
+        </span>
+        <span className="px-4 py-[0.75rem] rounded-r-md bg-[#2B2931]
+        text-white peer-checked:bg-[#44279A]">
+          {b}
+        </span>
+      </label>
+    </div>
+  )
+}
