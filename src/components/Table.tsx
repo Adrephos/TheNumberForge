e28de0ba137247580529a4a63570390bfa8d7b91
Adrephos <adrephos@gmail.com>
2024-05-01 18:16:40 -0500

    chore: change table colors

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,9 @@
 export const DataTable = ({ columns, rows }: { columns: Array<string>, rows: Array<number>[] }) => {
   return (
-    <div className="relative overflow-x-auto w-full">
-      <table className="w-full text-left rtl:text-right text-gray-500">
-        <thead className="text-gray-700 bg-gray-50">
-          <tr className="bg-slate-200 border-b">
+    <div className="relative overflow-x-auto w-full rounded-xl">
+      <table className="w-full text-left rtl:text-right text-gray-200">
+        <thead className="text-gray-300 bg-gray-50">
+          <tr className="bg-gray-800 border-b-4 border-gray-700">
             {columns.map((column) => (
               <th scope="col" className="px-6 py-3">
                 {column}
@@ -13,7 +13,7 @@ export const DataTable = ({ columns, rows }: { columns: Array<string>, rows: Arr
         </thead>
         <tbody>
           {rows.map((row) => (
-            <tr className="bg-white border-b">
+            <tr className="bg-gray-600 border-b border-gray-500">
               {row.map((cell, i) => (
                 <td scope="row" className="px-6 py-4">{
                   columns[i] == "error" && cell == 100
