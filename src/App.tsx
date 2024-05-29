11ba9566693756441d7006debbd67e45ba0cb12d
Adrephos <adrephos@gmail.com>
2024-05-29 13:03:24 -0500

    feat: add systems of equation and interpolation methods

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import './App.css'
 import { Home } from './pages/Home.tsx'
 import { Methods } from './pages/Methods.tsx'
 import { NonlinearMethod } from './pages/NonlinearMethod.tsx'
+import { SystemsMethod } from './pages/SystemsMethod.tsx'
 import { Plot } from './pages/Plot.tsx'
 import Error from './pages/Error.tsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Vandermonde } from './pages/Vandermonde.tsx'
+import { NewtonInterpolant } from './pages/NewtonInterpolant.tsx'
+import { Lagrange } from './pages/Lagrange.tsx'
+import { LinearSpline } from './pages/LinearSpline.tsx'
+import { CubicSpline } from './pages/CubicSpline.tsx'
 
 let router = createBrowserRouter([
   {
@@ -19,6 +25,30 @@ let router = createBrowserRouter([
   {
     path: "methods/nonlinear/:method",
     element: <NonlinearMethod />,
+  },
+  {
+    path: "methods/systems/:method",
+    element: <SystemsMethod />,
+  },
+  {
+    path: "methods/interpolation/vandermonde",
+    element: <Vandermonde />,
+  },
+  {
+    path: "methods/interpolation/newton",
+    element: <NewtonInterpolant />,
+  },
+  {
+    path: "methods/interpolation/lagrange",
+    element: <Lagrange />,
+  },
+  {
+    path: "methods/interpolation/linear_spline",
+    element: <LinearSpline />,
+  },
+  {
+    path: "methods/interpolation/cubic_spline",
+    element: <CubicSpline />,
   },
   {
     path: "methods/plot/plot_function",
